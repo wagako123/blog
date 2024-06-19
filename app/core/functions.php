9d770da9ba0e550325a0102875505cb576300443
unknown <brianwagako@gmail.com>
2024-06-19 14:23:30 +0300

    workingmysqlcon

--- app/core/functions.php
+++ app/core/functions.php
@@ -1,29 +1,39 @@
 <?php
 
-function create_tables (){
+// function create_tables (){
 
 
 $string="mysql:hostname=localhost;";
+try{
 $con = new PDO($string, DBUSER ,DBPASS);
-
-$query= "create database if not exists".DBNAME;
-$stm=$con-> prepare ($query);
-$stm->execute();
-
-$query= "create table if not exists users(
-
-    id int primary key auto_incriment,
-    user varchar (50) not null,
-    email varchar (100) not null,
-    password varchar(255) not null,
-    image varchar (1024) null,
-    date datetime default current_timestamp,
-    role varchar(10) not null,
-
-    key email (email),
-    key username (username)
-    )";
-$stm=$con-> prepare ($query);
-$stm->execute();
+echo "you have connected";
+}
+catch(PDOException $e)
+{
+    $error_message=$e ->getMessage();
+    echo $error_message;
+    exit();
 
 }
+
+// $query= "create database if not exists".DBNAME;
+// $stm=$con-> prepare ($query);
+// $stm->execute();
+
+// $query= "create table if not exists users(
+
+//     id int primary key auto_incriment,
+//     user varchar (50) not null,
+//     email varchar (100) not null,
+//     password varchar(255) not null,
+//     image varchar (1024) null,
+//     date datetime default current_timestamp,
+//     role varchar(10) not null,
+
+//     key email (email),
+//     key username (username)
+//     )";
+// $stm=$con-> prepare ($query);
+// $stm->execute();
+
+//}
